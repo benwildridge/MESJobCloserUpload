486dc185c2a50f52fad3069ad414a85c590d6082
Ben Wildridge <Ben.Wildridge@howdens.com>
2021-08-04 16:29:12 +0100

    Complete. Just Configurable connection info to do.

---
+++ MESJobCloser/DataWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Oracle.ManagedDataAccess.Client;
+
+namespace MESJobCloser
+{
+    /// <summary>
+    /// Interaction logic for DataWindow.xaml
+    /// </summary>
+    public partial class DataWindow : Window
+    {
+
+        OracleConnection con = new OracleConnection(ConfigurationManager.ConnectionStrings["dev"].ConnectionString);
+        OracleCommand com = new OracleCommand();
+
+        public string productionOrder = "";
+
+        public DataWindow()
+        {
+            InitializeComponent();
+            RetrieveDataGrid();
+        }
+
+        public DataTable RetrieveDataGrid()
+        {
+            con.Close();
+            con.Open();
+            com = con.CreateCommand();
+            com.CommandText = "SELECT PONUMBER, WORKCENTRE, MATERIAL, MATERIALDESCRIPTION, QTYREQUIRED FROM TBLPRODUCTIONLIST WHERE STATUSID ='2' AND WORKCENTRE = 'PACKAUT1'";
+            com.CommandType = System.Data.CommandType.Text;
+            OracleDataReader dr = com.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(dr);
+            gridOracleData.ItemsSource = dt.DefaultView;
+            dr.Close();
+            return dt;
+        }
+
+        private void RequestDataGridUpdate()
+        {
+            var productionOrderTable = RetrieveDataGrid();
+            
+            if (productionOrderTable.Rows.Count == 0)
+            {
+                MessageBox.Show("No active order found for PACKAUT1", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+        }
+
+        private void UpdateJobStatus(String sql_stmt)
+        {
+            
+            String msg = "Order Successfully Finished. Refresh MES Screen.";
+
+            if(productionOrder == "")
+            {
+                MessageBox.Show("No Order Selected", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else { 
+
+            com = con.CreateCommand();
+            com.CommandText = sql_stmt;
+            com.CommandType = CommandType.Text;
+            com.ExecuteNonQuery();
+            MessageBox.Show(msg, "Update Complete", MessageBoxButton.OK, MessageBoxImage.Information);
+            RetrieveDataGrid();
+            }
+        }
+
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void RetrieveData_Click(object sender, RoutedEventArgs e)
+        {
+            this.RequestDataGridUpdate();
+        }
+
+        private void BntLogout_Click(object sender, RoutedEventArgs e)
+        {
+            con.Close();
+            this.Close();
+        }
+
+        private void FinishOrder_Click(object sender, RoutedEventArgs e)
+        {
+            String sql = $"UPDATE TBLPRODUCTIONLIST SET STATUSID = '4' WHERE STATUSID = '2' AND WORKCENTRE = 'PACKAUT1' AND PONUMBER = '{productionOrder}'";
+            this.UpdateJobStatus(sql);
+            con.Close();
+        }
+
+        private void gridOracleData_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DataGrid dg = sender as DataGrid;
+            DataRowView dr = dg.SelectedItem as DataRowView;
+            if (dr != null)
+            {
+                productionOrder = dr["PONUMBER"].ToString();
+
+            }
+        }
+    }
+}
---
+++ MESJobCloser/LoginWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Oracle.ManagedDataAccess.Client;
+using System.Configuration;
+using System.Data;
+
+namespace MESJobCloser
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        OracleConnection con = new OracleConnection(ConfigurationManager.ConnectionStrings["dev"].ConnectionString);
+        OracleCommand com = new OracleCommand();
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string Username = txtUsername.Text.Trim();
+            string Password = txtPassword.Password.Trim();
+            con.Open();
+            com.Connection = con;
+            var oracleQuery = $"Select * from TBLCONSOLEUSER WHERE UPPER(USERNAME)='{Username.ToUpper()}' AND PASSWORD='{Password}'";
+            var test = new OracleCommand(oracleQuery,con);
+            
+            
+            try { OracleDataReader dr = test.ExecuteReader(); 
+                if(dr.HasRows)
+                {
+                    MessageBox.Show("Login Successful", "Congratulations", MessageBoxButton.OK, MessageBoxImage.Information);
+                    DataWindow dataDisplay = new DataWindow();
+                    dataDisplay.Show();
+                    this.Close();
+                    
+                }
+                else
+                {
+                    MessageBox.Show("Username/Password Incorrect", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    con.Close();
+                }
+            }
+            catch (Exception ex) { };
+        }
+
+
+
+
+        private void txtUserEnter(object sender, RoutedEventArgs e)
+        {
+            if (txtUsername.Text.Equals(@"Username"))
+            {
+                txtUsername.Text = "";
+            }
+        }
+
+        private void txtUserLeave(object sender, RoutedEventArgs e)
+        {
+            if (txtUsername.Text.Equals(""))
+            {
+                txtUsername.Text = @"Username";
+            }
+        }
+
+        private void txtPassEnter(object sender, RoutedEventArgs e)
+        {
+            if (txtPassword.Password.Equals("Password"))
+            {
+                txtPassword.Password = "";
+            }
+        }
+
+        private void txtPassLeave(object sender, RoutedEventArgs e)
+        {
+            if (txtPassword.Password.Equals(""))
+            {
+                txtPassword.Password = "Password";
+            }
+        }
+
+        private void BtnExit_Click(object sender, RoutedEventArgs e)
+        {
+            con.Close();
+            this.Close();
+        }
+    }
+}
+
